c0368234595c2a2f6932066135194c0281b3f7ac
Hung Le <hung.le@bluecomgroup.com>
2016-05-25 19:17:11 +0700

    Chapter 6 and 7 - Done but has bug when created user or logged user

--- js/views/appHeader.jsx
+++ js/views/appHeader.jsx
@@ -1,13 +1,40 @@
-import React from 'react';
-import { Link } from 'react-router';
+"use strict";
+
+import React             from 'react';
+import Reflux            from 'reflux';
+import { Link, browserHistory } from 'react-router';
+import Actions           from 'appRoot/actions';
+import SessionStore      from 'appRoot/stores/sessionContext';
 
 export default React.createClass({
-  render: function() {
-    return (
-      <header className="app-header">
-        app header
-        <Link to="/login">Log In</Link>
-      </header>
-    );
-  }
+	mixins: [
+		Reflux.connect(SessionStore, 'session'),
+		// History
+	],
+	logOut: function () {
+		Actions.logOut();
+		// this.history.pushState('', '/');
+    browserHistory.push('', '/');
+	},
+	render: function () {
+		return (
+			<header className="app-header">
+				<Link to="/"><h1>Re&#923;ction</h1></Link>
+				<section className="account-ctrl">
+					{
+						this.state.session.loggedIn ?
+							(<Link to="/posts/create">
+								Hello {this.state.session.username}, write something!
+							</Link>) :
+							<Link to="/users/create">Join</Link>
+					}
+					{
+						this.state.session.loggedIn ?
+							<a onClick={this.logOut}>Log Out</a> :
+							<Link to="/login">Log In</Link>
+					}
+				</section>
+			</header>
+		);
+	}
 });
